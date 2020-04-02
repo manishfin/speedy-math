09eb74586d84ca9caf926168daf468879cc00430
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-04-02 23:14:41 +0530

    Add utils functions

---
+++ src/utils/utils.js
@@ -0,0 +1,27 @@
+
+const operators = ['+', '-', '*', '/'];
+
+const generateRandomNumber = (min=1, max=100) => Math.floor(Math.random() * (max - min) + min);
+
+export const round = (value, place=0) => Math.round(value * Math.pow(10, place)) / Math.pow(10, place);
+
+export const getRandomValue = (values) => {
+    if (!(Array.isArray(values) && values.length)) return;
+
+    const index = generateRandomNumber(0, values.length);
+    return values[index];
+}
+
+export const generateQuestionAndAnswer = () => {
+    const leftNum = generateRandomNumber();
+    const rightNum = generateRandomNumber();
+    const operator = getRandomValue(operators);
+    const question = `${leftNum} ${operator} ${rightNum}`;
+    const answer = round(eval(question), 1);
+
+    return {
+        question,
+        answer,
+    };
+};
+
