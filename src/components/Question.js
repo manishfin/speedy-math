551dc938a1be3d51953c01a304dd16e842f640ad
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-04-02 23:20:43 +0530

    [Question] Removed answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -74,7 +74,6 @@ class Question extends Component {
                             );
                         })
                     }
-                    {answer}
                 </div>
             </Card>
         );
