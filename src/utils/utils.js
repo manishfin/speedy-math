f9199a5f19b625cb40a1581853a80516c0936d95
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-04-03 11:42:05 +0530

    [utils] Add randomization

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,8 @@
 
 const operators = ['+', '-', '*', '/'];
 
+const digit = [10, 100];
+
 export const generateRandomNumber = (min=1, max=100) => Math.floor(Math.random() * (max - min) + min);
 
 export const round = (value, place=0) => Math.round(value * Math.pow(10, place)) / Math.pow(10, place);
@@ -13,8 +15,8 @@ export const getRandomValue = (values) => {
 }
 
 export const generateQuestionAndAnswer = () => {
-    const leftNum = generateRandomNumber();
-    const rightNum = generateRandomNumber();
+    const leftNum = generateRandomNumber(1, getRandomValue(digit));
+    const rightNum = generateRandomNumber(1, getRandomValue(digit));
     const operator = getRandomValue(operators);
     const question = `${leftNum} ${operator} ${rightNum}`;
     const answer = round(eval(question), 1);
